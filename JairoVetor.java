2bb2428f0d0e27bb329357457da2cda8b5ef1ca1
ItsTheDuds <128929388+ItsTheDuds@users.noreply.github.com>
2026-01-20 00:45:41 +0000

    Começando estudo de vetores

---
+++ JairoVetor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class JairoVetor {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos alunos tem na sala de aula: ");
+        int qtdAlunos = sc.nextInt();
+        System.out.println();
+        sc.nextLine();
+
+        String[] nomes = new String[qtdAlunos];
+        int[] idades = new int[qtdAlunos];
+
+        int[] alunos = new int [qtdAlunos];
+
+        for (int i = 0; i < qtdAlunos; i++) {
+            System.out.println("Qual nome do aluno?");
+            nomes[i] = sc.nextLine();
+            System.out.println("Qual idade do aluno?");
+            idades[i] = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < qtdAlunos; i++) {
+            System.out.println("NOME DO ALUNO: " + nomes[i] + " - " + idades[i] + "ANOS");
+        }
+
+        sc.close();
+    }
+}
